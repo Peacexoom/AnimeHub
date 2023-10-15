a2f239d086e7b89b00a14a20db92633e0baa2eba
Irfan <irfansalim77@gmail.com>
2023-10-15 12:02:42 +0530

    Updated the hard coded values to env variables

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -77,9 +77,9 @@ const sendMail = (name, email, token) => {
               <div class="container">
                 <h1>Hello ${name},</h1>
                 <p>Thank you for signing up with AnimeHub! To get started, please verify your email by clicking the button below:</p>
-                <a href="http://localhost:5000/verify/${token}">Verify Your Email</a>
+                <a href="${process.env.BACKEND}/verify/${token}">Verify Your Email</a>
                 <p>If the button above doesn't work, you can also copy and paste the following link into your web browser:</p>
-                <p><a href="http://localhost:5000/verify/${token}">http://localhost:5000/verify/${token}</a></p>
+                <p><a href="${process.env.BACKEND}/verify/${token}">${process.env.BACKEND}/verify/${token}</a></p>
                 <p>If you didn't sign up for an AnimeHub account, please disregard this email.</p>
               </div>
             </body>
@@ -164,7 +164,7 @@ app.get("/verify/:token", async (req, res) => {
             await db.query(updateSql, [userId]);
 
             // Redirect to the login page (adjust the URL as needed)
-            return res.redirect("http://localhost:5173/login");
+            return res.redirect(`${process.env.FRONTEND}/login`);
         } else throw "Invalid token";
     } catch (error) {
         next(error);
