33ccdfbb8dbdeb4824227b600b4a53dd97923fbf
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-10-18 13:08:50 +0530

    refactor: email templates

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,6 +11,7 @@ async function main(db) {
     const crypto = require("crypto");
     const nodemailer = require("nodemailer");
     const bcrypt = require("bcrypt");
+    const emailTemplates = require("./emailTemplates/index.js");
     // const morgan = require("morgan");
     const { check, validationResult } = require("express-validator");
     const port = 5000;
@@ -49,53 +50,7 @@ async function main(db) {
             from: process.env.EMAIL,
             to: email,
             subject: "AnimeHub Account Verification",
-            html: `
-            <html>
-                <head>
-                <style>
-                    /* Add CSS styles for better email formatting */
-                    body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f4f4f4;
-                    text-align: center;
-                    }
-                    .container {
-                    max-width: 600px;
-                    margin: 0 auto;
-                    padding: 20px;
-                    background-color: #fff;
-                    border-radius: 10px;
-                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                    }
-                    h1 {
-                    color: #333;
-                    }
-                    p {
-                    font-size: 16px;
-                    }
-                    a {
-                    display: inline-block;
-                    padding: 10px 20px;
-                    margin: 20px 0;
-                    background-color: #007BFF;
-                    color: #fff !important;
-                    text-decoration: none;
-                    border-radius: 5px;
-                    }
-                </style>
-                </head>
-                <body>
-                <div class="container">
-                    <h1>Hello ${name},</h1>
-                    <p>Thank you for signing up with AnimeHub! To get started, please verify your email by clicking the button below:</p>
-                    <a href="${process.env.BACKEND}/verify/${token}">Verify Your Email</a>
-                    <p>If the button above doesn't work, you can also copy and paste the following link into your web browser:</p>
-                    <p><a href="${process.env.BACKEND}/verify/${token}">${process.env.BACKEND}/verify/${token}</a></p>
-                    <p>If you didn't sign up for an AnimeHub account, please disregard this email.</p>
-                </div>
-                </body>
-            </html>
-            `,
+            html: emailTemplates.accountVerification(name, token),
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
